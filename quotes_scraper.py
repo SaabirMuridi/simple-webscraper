8b0c342092882ff9c8870397ca97bae9d6a8e48b
SaabirMuridi <saabir.m10@gmail.com>
2024-02-06 17:11:15 -0500

    Created funtion to scrape quotes from multiple pages of the url using BeautifulSoup and used requests to ask for permission I also created a while loop within the main body to format the pages of quotes from the first three pages

---
+++ quotes_scraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def quotes_scraper(url):
+    #variable will hold scraped content
+    quotes = []
+    while url:
+        try:
+            #request response from url
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            #content extraction from current page
+            current_page_quotes = soup.select('.quote span.text')
+            quotes.extend(current_page_quotes)
+
+            #finds next page using the sites 'next' button
+            next_page_link = soup.select_one('li.next a')
+            url = next_page_link['href'] if next_page_link else None
+        except Exception as e:
+            print("Error:", e)
+            break
+            
+    return quotes
+
+if __name__ == "__main__":
+    base_url = "https://quotes.toscrape.com/page/{}/"
+    for page_number in range(1,4):
+        url = base_url.format(page_number)
+        quotes = quotes_scraper(url)
+        print(f"Quotes from page {page_number}:")
+        for quote in quotes:
+            print("-", quote.text.strip())
